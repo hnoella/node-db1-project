1d5b0881ee05e901a039b6adb64c11de1988af29
Noella Hirwa <noellahirwa@Jacquelines-Air.localdomain>
2022-01-17 11:16:53 -0600

    first commit

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,69 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const md = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json("get accounts");
+  } catch (err) {
+    next({ status: 422, message: "this is horrible" });
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", md.checkAccountID, (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    res.json("get account by id");
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post(
+  "/",
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("post account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  md.checkAccountID,
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("update account");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", md.checkAccountID, (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    res.json("delete account");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+  next();
+  // DO YOUR MAGIC
+});
 
 module.exports = router;
